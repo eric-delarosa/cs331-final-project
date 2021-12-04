ee3d1e0eac2b37b3da5d10ed49ef357ccd771e82
eric-delarosa <ericd.delarosa@gmail.com>
2021-12-03 19:26:45 -0500

    created employee and medication tabel

---
+++ Employee.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Employee(
+    id INT PRIMARY KEY NOT NULL,
+    name_last VARCHAR(255) NOT NULL,
+    name_first VARCHAR(255) NOT NULL,
+    deptID INT NOT NULL,
+    FOREIGN KEY(deptID) REFERENCES Department(id)
+);
---
+++ Medication.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Medication(
+    TradeName VARCHAR(255) PRIMARY KEY NOT NULL,
+    Stock INT NOT NULL,
+    Treats CLOB NOT NULL,
+    Formula VARCHAR(255) NOT NULL,
+    SideEffects CLOB NOT NULL
+);
